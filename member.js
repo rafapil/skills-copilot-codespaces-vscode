01bc17c00c5ffbb33e01ef914224381763ea9d94
Rafael Silva <rafael.filgue@gmail.com>
2024-06-20 01:58:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var memberSkills = ['Javascript', 'HTML', 'CSS', 'React', 'NodeJS'];
+
+    for (var i = 0; i < memberSkills.length; i++) {
+        console.log(memberSkills[i]);
+    }
+}
